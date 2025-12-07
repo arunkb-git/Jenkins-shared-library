b97d9f1413ffa3d664ec607ff20881ca9cc26bb7
arunkb-git <arun.charith@gmail.com>
2025-12-07 20:38:40 +0530

    Update tomcat_deploy.groovy

--- vars/tomcat_deploy.groovy
+++ vars/tomcat_deploy.groovy
@@ -1,4 +1,4 @@
 def call(servername,artifact_name){
- sshPublisher(publishers: [sshPublisherDesc(configName: ${servername}, transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''/opt/tomcat11/bin/shutdown.sh
+ sshPublisher(publishers: [sshPublisherDesc(configName: "${servername}", transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''/opt/tomcat11/bin/shutdown.sh
 /opt/tomcat11/bin/startup.sh''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: 'target', sourceFiles: 'target/${artifact_name}.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)]) 
 }
